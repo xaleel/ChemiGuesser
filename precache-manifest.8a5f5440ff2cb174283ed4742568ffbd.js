20be355a653813f7aafe83398aef676085e5caac
Xaleel <xaleelchess@gmail.com>
2022-07-02 20:12:10 +0300

    Updates

---
+++ precache-manifest.8a5f5440ff2cb174283ed4742568ffbd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "23a0866e0591128eb631",
+    "url": "/ChemiGuesser/static/css/main.0d581621.chunk.css"
+  },
+  {
+    "revision": "23a0866e0591128eb631",
+    "url": "/ChemiGuesser/static/js/main.23a0866e.chunk.js"
+  },
+  {
+    "revision": "300ccace53ceddbd09ba",
+    "url": "/ChemiGuesser/static/js/1.300ccace.chunk.js"
+  },
+  {
+    "revision": "ba5f6766f2e12d87aaca",
+    "url": "/ChemiGuesser/static/js/runtime~main.ba5f6766.js"
+  },
+  {
+    "revision": "d0093a6ff102aaae53b1333b41d46ddb",
+    "url": "/ChemiGuesser/index.html"
+  }
+];
